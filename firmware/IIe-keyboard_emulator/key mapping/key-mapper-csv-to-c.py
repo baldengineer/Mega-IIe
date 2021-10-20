d1b120cbf87e15968ee40b29cc087bb3b9fb5910
baldengineer <james@baldengineer.com>
2021-10-20 14:53:53 -0500

    modified python mapper script to make c-ready code. just copy and paste.

--- firmware/IIe-keyboard_emulator/key mapping/key-mapper-csv-to-c.py
+++ firmware/IIe-keyboard_emulator/key mapping/key-mapper-csv-to-c.py
@@ -6,18 +6,24 @@ def get_field(col):
 	f = open("mapping-rev1.csv")
 	counter = 0
 	for x in f:
-		x = x.strip()
-		fields = x.split(",")
-		print(fields[col] + ",", end='')
-		if (counter % 8 == 0):
-			print("")
+		if (counter > 0):
+			x = x.strip()
+			fields = x.split(",")
+			print(fields[col] + ",", end='')
+			if (counter % 8 == 0):
+				print("")
 		counter = counter + 1
 	f.close()
 
+print("uint8_t normal_kbd_map[] = {")
 get_field(2)
-print("-"*10)
+
+print("};\n\nuint8_t ctrl_kbd_map[] = {")
 get_field(3)
-print("--------------")
+
+print("};\n\nuint8_t shift_kbd_map[] = {")
 get_field(4)
-print("--------------")
+
+print("};\n\nuint8_t both_kbd_map[] = {")
 get_field(5)
+print("};\n\n//", end='')
