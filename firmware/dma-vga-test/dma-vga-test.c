c8dc2c5a657aada8d6347c0b0c2791a81421af82
James Lewis <byerly0503@gmail.com>
2022-04-22 17:31:49 -0500

    initial commit for code to test DMAs with VGA2040

---
+++ firmware/dma-vga-test/dma-vga-test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/dma.h"
+#include "hardware/pio.h"
+
+
+
+int main()
+{
+    stdio_init_all();
+
+
+
+    puts("Hello, world!");
+
+    return 0;
+}
