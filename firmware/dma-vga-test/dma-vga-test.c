6c56793899b2d56c6e2e69f06e621362a17f380b
James Lewis <byerly0503@gmail.com>
2022-04-24 15:59:34 -0500

    capturing traffic from patt gen

--- firmware/dma-vga-test/dma-vga-test.c
+++ firmware/dma-vga-test/dma-vga-test.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "pico/stdlib.h"
 #include "hardware/dma.h"
 #include "hardware/pio.h"
 #include "hardware/structs/bus_ctrl.h"
+#include "pico/stdlib.h"
+
+#include <tusb.h>
 
 #include "TEST_CAP.pio.h"
 
@@ -12,6 +14,14 @@ PIO pio;
 uint pio_offset;
 uint pio_sm;
 uint rgb_dma_chan;
+
+#define LED_OFF 0
+#define LED_ON  1
+#define LED_TOG 2
+
+const uint LED_PIN = PICO_DEFAULT_LED_PIN; // 3 on VGA2040
+bool led_pin_state = LED_ON;
+
 
 void TEST_CAP_pio_init() {
     // do the setup things
@@ -23,6 +33,8 @@ void TEST_CAP_pio_init() {
     pio_sm_set_enabled(pio, pio_sm, false);
 
     sm_config_set_in_pins(&c, 12); // capture 7M too, because why not?
+    pio_sm_set_consecutive_pindirs(pio, pio_sm, 12, 8, GPIO_IN);
+
     //sm_config_set_clkdiv(&c, ???); // LA example reduces sample rate with this
 
     // From C SDK PDF:
@@ -55,34 +67,69 @@ void TEST_CAP_pio_arm(uint32_t *capture_buf, size_t capture_size_words) {
     pio_sm_set_enabled(pio, pio_sm, true);
 }
 
-void print_capture_buf(const uint32_t *buf) {
+void print_capture_buf(const uint32_t *buf, uint word_count) {
     printf("Capture: lol\n");
-    // Each FIFO record may be only partially filled with bits, depending on
-    // whether pin_count is a factor of 32.
-    // uint record_size_bits = 32;
-    // for (int sample = 0; sample < n_samples; ++sample) {
-    //     uint word_index = bit_index / record_size_bits;
-    //     // Data is left-justified in each FIFO entry, hence the (32 - record_size_bits) offset
-    //     uint word_mask = 1u << (bit_index % record_size_bits + 32 - record_size_bits);
-    //     printf(buf[word_index] & word_mask ? "." : " ");
-    // }
-    // printf("\n");
+    for (int x=0; x < word_count; x++) {
+        printf("%d: %#08X, b%b, ", x, buf[x], buf[x]); // lol, %b works
+
+        // extact RGB8,4,2,1 for printing
+        for (int j=0; j<4; j++) {
+            uint offset = 8 * j;
+            printf((buf[x] & (0x1<<4+(offset))) ? "1" : "0");
+            printf((buf[x] & (0x1<<5+(offset))) ? "1" : "0");
+            printf((buf[x] & (0x1<<2+(offset))) ? "1" : "0");
+            printf((buf[x] & (0x1<<3+(offset))) ? "1" : "0");
+            printf((j!=3) ? ", " : "");
+        } 
+        printf("\n");
+    }
+    printf("\n");
+}
+
+uint ctrl_status_led(uint state) {
+    switch (state) {
+        case LED_OFF:
+            led_pin_state = LED_OFF;
+            break;
+        case LED_ON:
+            led_pin_state = LED_ON;
+            break;
+        case LED_TOG:
+            led_pin_state = !led_pin_state;
+            break;
+    }
+    gpio_put(LED_PIN, led_pin_state);
+    return led_pin_state;
 }
 
 
 int main() {
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    ctrl_status_led(LED_ON);
     stdio_init_all();
+    while(!tud_cdc_connected()) { 
+        sleep_ms(100);
+        ctrl_status_led(LED_TOG);
+    }
+    sleep_ms(100);
+    printf("Hello World...\n");
+    ctrl_status_led(LED_OFF);
 
     printf("\n[Init] TEST_CAP Pio...");
     TEST_CAP_pio_init();
     printf("done!");
 
     puts("\n\n----\nDMA VGA Test.");
+    for (int x; x<5; x++) {
+        printf("Wait %d\n");
+        busy_wait_us(1000000);
+    }
 
     while(1) {
-        puts("Starting capture (breakpoint!)");
+        puts("Starting capture");
         // create buffer for a line
-        uint buf_size_words = 560;
+        uint buf_size_words = 17;
         uint32_t *capture_buf = malloc(buf_size_words * sizeof(uint32_t));
         hard_assert(capture_buf); // did we get buffer?
 
@@ -92,11 +139,13 @@ int main() {
         // not entirely sure when this is needed, but will leave here just in case.
         // bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
 
-        TEST_CAP_pio_arm(capture_buf, buf_size_words);
+        TEST_CAP_pio_arm(capture_buf, 17);
 
         dma_channel_wait_for_finish_blocking(rgb_dma_chan); // not sure I did the maths right...
 
-        print_capture_buf(capture_buf);
+        print_capture_buf(capture_buf, 17);
+        puts("Breakpoint here...");
+        while(1);
     }
 
 
